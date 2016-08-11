6b8865c0813618176ae398326bc8cc93dd69876d
Taylor Harry <taylor.andrew.harry@gmail.com>
2016-08-11 18:33:36 -0500

    added final JS file

--- coins.js
+++ coins.js
@@ -8,40 +8,32 @@ var penny = 1;
 var numOfCoins = 0;
 
 function coinCounter (dollarAmt) {
-    var coinPurse = {};
+    var coinPurse = {
+      Quarter: 0,
+      Dime: 0,
+      Nickel: 0,
+      Penny: 0
+    };
+    var quarter = 25;
+    var dime = 10;
+    var nickel = 5;
+    var penny = 1;
+    var numOfCoins = 0;
 
     dollarAmt = dollarAmt*100;
 
-  while (dollarAmt >= quarter) {
-    dollarAmt -= quarter;
-    numOfCoins++;
-  }
-  coinPurse.quarters = numOfCoins;
-  numOfCoins = 0;
+    coinPurse.Quarter = Math.floor(dollarAmt/quarter);
+    dollarAmt = dollarAmt - (coinPurse.Quarter * quarter);
 
+    coinPurse.Dime = Math.floor(dollarAmt/dime);
+    dollarAmt = dollarAmt - (coinPurse.Dime * dime);
 
-  while (dollarAmt >= dime) {
-    dollarAmt -= dime;
-    numOfCoins++;
-  }
-  coinPurse.dimes = numOfCoins;
-  numOfCoins = 0;
+    coinPurse.Nickel = Math.floor(dollarAmt/nickel);
+    dollarAmt = dollarAmt - (coinPurse.Nickel * nickel);
 
+    coinPurse.Penny = Math.floor(dollarAmt/penny);
+    dollarAmt = dollarAmt - (coinPurse.Penny * penny);
 
-  while (dollarAmt >= nickel) {
-    dollarAmt -= nickel;
-    numOfCoins++;
-  }
-  coinPurse.nickels = numOfCoins;
-  numOfCoins = 0;
-
-
-  while (dollarAmt >= penny) {
-    dollarAmt -= penny;
-    numOfCoins++;
-  }
-  coinPurse.pennies = numOfCoins;
-  numOfCoins = 0;
 
   return coinPurse;
 }
